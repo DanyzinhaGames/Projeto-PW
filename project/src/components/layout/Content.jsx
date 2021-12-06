c3455752e2a82cf2f52bf30742dbae6e7157bc64
DanyzinhaGames <Danielly.santos36@etec.sp.gov.br>
2021-12-06 16:48:33 +0000

    help

--- project/src/components/layout/Content.jsx
+++ project/src/components/layout/Content.jsx
@@ -1,6 +1,6 @@
 import './Content.css'
 import React from "react"
-import { Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import Inicio from '../../views/contents/Inicio'
 import Detalhes from '../../views/contents/Detalhes'
 import Sobre from '../../views/contents/Sobre'
